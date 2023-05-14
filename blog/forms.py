382fc392401c19fbd4a081c514e064d9c7c8654e
ScottA27 <scotta27501@gmail.com>
2023-05-14 19:02:48 +0000

    Added the ability to edit posts

--- blog/forms.py
+++ blog/forms.py
@@ -15,3 +15,18 @@ class PostForm(forms.ModelForm):
             'featured_image': forms.FileInput(attrs={'class': 'form-control'}),
             'status': forms.Select(attrs={'class': 'form-control'}),
         }
+
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'content', 'featured_image', 'status')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'featured_image': forms.FileInput(attrs={'class': 'form-control'}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
+        }
