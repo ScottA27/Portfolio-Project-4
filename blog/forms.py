512379ff0800318ebbba388afde58dd1e3d81c12
ScottA27 <scotta27501@gmail.com>
2023-05-14 18:41:30 +0000

    Added basic bootstrap styling to the add post page

---
+++ blog/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'author', 'content', 'featured_image', 'status')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'featured_image': forms.FileInput(attrs={'class': 'form-control'}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
+        }
