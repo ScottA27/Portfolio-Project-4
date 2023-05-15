5ee7d17b433f45fb34202206d9d240406f03d338
ScottA27 <scotta27501@gmail.com>
2023-05-15 19:37:12 +0000

    Added the date and time when posts were made. Also, added the ability to delete posts

--- blog/forms.py
+++ blog/forms.py
@@ -25,7 +25,7 @@ class EditForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
+            # 'author': forms.Select(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control'}),
             'featured_image': forms.FileInput(attrs={'class': 'form-control'}),
             'status': forms.Select(attrs={'class': 'form-control'}),
