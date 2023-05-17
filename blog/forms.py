453da89f03b171be00dbb4ba72e4a9ac70983628
ScottA27 <scotta27501@gmail.com>
2023-05-17 21:01:27 +0000

    Added categories to the blog, as well as allowing users to create new categories

--- blog/forms.py
+++ blog/forms.py
@@ -1,16 +1,25 @@
 from django import forms
-from .models import Post
+from .models import Post, Category
+
+
+choices = Category.objects.all().values_list('name', 'name')
+
+choice_list = []
+
+for i in choices:
+    choice_list.append(i)
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'content', 'featured_image', 'status')
+        fields = ('title', 'title_tag', 'author', 'category', 'content', 'featured_image', 'status')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control'}),
             'featured_image': forms.FileInput(attrs={'class': 'form-control'}),
             'status': forms.Select(attrs={'class': 'form-control'}),
--- blog/models.py
+++ blog/models.py
@@ -9,8 +9,8 @@ STATUS = ((0, 'Draft'), (1, 'Published'))
 
 class Post(models.Model):
     ''' The model used for the posts on the blog'''
-    title = models.CharField(max_length=200, unique=True)
-    title_tag = models.CharField(max_length=200, unique=True)
+    title = models.CharField(max_length=200)
+    title_tag = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     updated_on = models.DateTimeField(auto_now=True)
@@ -19,6 +19,7 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='blogpost_like', blank=True)
+    category = models.CharField(max_length=200, default='uncategorised')
 
     class Meta:
         ordering = ["-created_on"]
@@ -47,3 +48,14 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
+
+
+class Category(models.Model):
+    '''The model used for the different categories of blog posts'''
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('home')
