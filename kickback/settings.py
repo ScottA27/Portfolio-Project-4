a2a156506d9e8c16c55a7e66de0f96ce4f5eccb7
ScottA27 <scotta27501@gmail.com>
2023-06-02 16:08:55 +0000

    Updated settings.py file

--- kickback/settings.py
+++ kickback/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kickback-blog-pp4.herokuapp.com', 'localhost', '8000-scotta27-portfolioproje-6j51kf1hjhz.ws-eu98.gitpod.io']
+ALLOWED_HOSTS = ['kickback-blog-pp4.herokuapp.com', '8000-scotta27-portfolioproje-6j51kf1hjhz.ws-eu98.gitpod.io']
 
 
 # Application definition
